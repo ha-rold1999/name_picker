756886eec3d6a93be143bd5718bc10721ff51b36
JayMar921 <jayharronabejar@gmail.com>
2023-11-21 13:45:12 +0800

    feat: Download Excel will now fetch data from API

--- src/ExcelExport.jsx
+++ src/ExcelExport.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import * as XLSX from "xlsx";
 import * as FileSaver from "file-saver";
+import { GetWinners } from "./Util/Database";
 
 export default function ExcelExport() {
   const winners = JSON.parse(localStorage.getItem("winnersList"));
@@ -10,7 +11,10 @@ export default function ExcelExport() {
   const fileExtension = ".xlsx";
 
   const exportToExcel = async () => {
-    const ws = XLSX.utils.json_to_sheet(winners);
+    let winnersFromAPI = await GetWinners();
+    //console.log(winnersFromAPI);
+    //const ws = XLSX.utils.json_to_sheet(winners);
+    const ws = XLSX.utils.json_to_sheet(winnersFromAPI);
     const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
     const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
     const data = new Blob([excelBuffer], { type: filetype });
